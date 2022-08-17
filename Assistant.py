1bbc58c6729152807d183f25cc9e42cb7f3dc5d0
Vihar <202001110@daiict.ac.in>
2022-08-17 20:20:26 +0530

    open youtube, chrome, wikipedia, google search
    Assistant query execution for opening youtube, and chrome, searching in wikipedia, and google done.
    run command created to run the assistant, close command to close the assistant(deletes the voice engine, recorder(recognizer), and other variables).
    setProperties command created sets properties of energy_threshold, phase_threhold, pause_threshold, non_speaking_duration for the recognizer based on users choice.

--- Assistant.py
+++ Assistant.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python
 
 from datetime import datetime
+from os import system
 import re
 import webbrowser
 import pyttsx3
 import speech_recognition as SR
 import wikipedia
-from googlesearch import search
+import googlesearch
 
 class Assistant:
     def __init__(self) -> None:
         '''Creates an instance of the Voice-Assistant, initiates the engine, and assigns one of the preconfigured voice to it'''
         self.__engine = pyttsx3.init('sapi5')
         self.__engine.setProperty('voice', self.__engine.getProperty('voices')[2].id)
+        
         self.__Terminate = False
+        self.__recognizer = SR.Recognizer()
+        
+        self.__recognizer.energy_threshold = 200
+        self.__recognizer.pause_threshold = 1
+        self.__recognizer.phrase_threshold = 0.3
+        self.__recognizer.non_speaking_duration = 0.5
 
     def __speak(self, text):
         '''Tells Assistant to speak the given text and also prints on the console'''
@@ -34,15 +42,12 @@ class Assistant:
         
     def __listenQuery(self):
         '''Listens for microphone input and returns string of the input'''
-        recognizer = SR.Recognizer()
         with SR.Microphone() as source:
             print("\nListening...")
-            recognizer.pause_threshold = 1 # Minimum no-audio time to consider phrase complete (default is 0.8)
-            recognizer.energy_threshold = 200
-            audio = recognizer.listen(source)
+            audio = self.__recognizer.listen(source)
         try: 
             print("Recognizing...\n")
-            query = recognizer.recognize_google(audio, language="en-in") # language = English(en)-India(in)
+            query = self.__recognizer.recognize_google(audio, language="en-in") # language = English(en)-India(in)
             query = query
             print(f"user said: {query}")
         except:
@@ -56,17 +61,98 @@ class Assistant:
         if not query:
             return
         
-        if any( text in query for text in ['exit assistant', 'quit assistant'] ):
+        if any( text in query for text in ['exit', 'quit'] ):
             self.__Terminate = True
+        
+        elif re.match(r'search .* in google', query):
+            searchQuery = re.findall(r'search (.*) in google', query)[0]
+            
+            if not searchQuery:
+                self.__speak("Invalid Google Search Query Found!!")
+                return
+            
+            results = googlesearch.search(term=searchQuery)
+            if results:
+                results = list(results)
+                self.__speak("Found Following Results: ")
+                
+                for i in range(len(results)):
+                    print(i+1, ")", results[i])
+            else:
+                self.__speak("No Search Result Found!!")
+                    
+        elif 'wikipedia' in query:
+            try:
+                query = query.replace('wikipedia', "")
+                self.__speak('Searching Wikipedia...')
+                results = wikipedia.summary(query,  sentences=3)
+                
+                self.__speak("According to Wikipedia...")
+                self.__speak(results)
+            except wikipedia.exceptions.DisambiguationError as DE:
+                self.__speak(f"\nError Found: {DE.__class__.__name__}")
+                
+                options = str(DE).split("\n")
+                if len(options) < 7:
+                    for option in options:
+                        self.__speak(option)
+                else:
+                    for option in options[:6]:
+                        self.__speak(option)
+                    self.__speak("... and more")
+                       
+        elif 'open youtube' in query:
+            self.__speak('opening youtube in chrome...')
+            webbrowser.open_new('https://www.youtube.com')
+            
+        elif 'open chrome' in query:
+            self.__speak('opening chrome...')
+            webbrowser.open_new('https://www.google.com')
+            
+        elif 'open google' in query:
+            query = query.replace('open google', "")
+            webbrowser.open_new('https://www.google.com')   
+        
+        else:
+            self.__speak('could not interprete the query')
         
            
     def run(self):
+        '''Initiates the assistant listening and executing query process in a loop until user asks to exit'''
+        system("clear")
         self.__wishUser()
         while not self.__Terminate:
             self.__executeQuery( self.__listenQuery() )
 
 
+    def setProperties(self, energy_threshold:int|None=None, pause_threshold:float|None=None, phrase_threshold:float|None=None, non_speaking_duration:float|None=None):
+        '''
+        Set properties for the listening
+
+        - energy_threshold: Minimum audio energy to consider for recording (default = 200)
+        - pause_threshold: Seconds of non-speaking audio to conclude a phrase (default = 1)
+        - phrase_threshold: Minimum seconds of speaking required to be considered as a phrase (audio with time less than this are ignored to rule out clicks and pops) (default = 0.3)
+        - non_speaking_duration: seconds of non-speaking audio to keep on both the sides of the recording (default = 0.5)
+        '''
+        if energy_threshold:
+            self.__recognizer.energy_threshold = energy_threshold
+        if pause_threshold:
+            self.__recognizer.pause_threshold = pause_threshold
+        if phrase_threshold:
+            self.__recognizer.phrase_threshold = phrase_threshold
+        if non_speaking_duration:
+            self.__recognizer.non_speaking_duration = non_speaking_duration
+            
+             
+    
+    def close(self):
+        '''Deletes the assistant's voice engine, recorder (recognizer) and the rest of the variables'''
+        del self.__engine
+        del self.__recognizer
+        del self.__Terminate
+    
 
 if __name__ == "__main__":
     assistant = Assistant()
-    assistant.run()
+    assistant._Assistant__executeQuery("search  in google")
+    assistant.close()
